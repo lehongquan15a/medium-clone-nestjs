cb46d96b363361a0cc053dd9cbe88cfcbea2b9c0
Le Hong Quan <lhquan1@cmcglobal.vn>
2022-06-09 06:35:15 +0700

    Nestjs: config postgres db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import { TagModule } from './tag/tag.module';
+import { TagModule } from '@app/tag/tag.module';
 import { Module } from '@nestjs/common';
 import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormconfig from '@app/ormconfig';
 
 @Module({
-  imports: [TagModule],
+  imports: [TypeOrmModule.forRoot(ormconfig), TagModule],
   controllers: [AppController],
   providers: [AppService],
 })
